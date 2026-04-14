f102901d97ce78ddc81791599052812c0f459745
haroldjunior501-cell <haroldjunior501@gmail.com>
2026-04-14 11:57:50 +0200

    Fix standalone balance compilation

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,6 +1,32 @@
 #include "binary_trees.h"
 
 /**
+ * tree_height - measures the height of a binary tree
+ * @tree: pointer to the root node of the tree
+ *
+ * Return: height of the tree, or 0 if tree is NULL
+ */
+static size_t tree_height(const binary_tree_t *tree)
+{
+	size_t left_height;
+	size_t right_height;
+
+	if (tree == NULL)
+		return (0);
+
+	if (tree->left == NULL && tree->right == NULL)
+		return (0);
+
+	left_height = tree_height(tree->left);
+	right_height = tree_height(tree->right);
+
+	if (left_height > right_height)
+		return (left_height + 1);
+
+	return (right_height + 1);
+}
+
+/**
  * binary_tree_balance - measures the balance factor of a binary tree
  * @tree: pointer to the root node of the tree to measure the balance factor
  *
@@ -18,9 +44,9 @@ int binary_tree_balance(const binary_tree_t *tree)
 	right_height = 0;
 
 	if (tree->left != NULL)
-		left_height = binary_tree_height(tree->left) + 1;
+		left_height = tree_height(tree->left) + 1;
 	if (tree->right != NULL)
-		right_height = binary_tree_height(tree->right) + 1;
+		right_height = tree_height(tree->right) + 1;
 
 	return (left_height - right_height);
 }
